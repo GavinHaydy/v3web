3d11414c3067ae3bfe3443cbe287e382e678ad09
bugpz <13558622779@163.com>
2021-08-30 23:05:05 +0800

    add：axios封装及拦截器配置

---
+++ src/utils/request.ts
@@ -0,0 +1,44 @@
+/*
+ * @Description:request.ts
+ * @CreatedBy:WebStorm
+ * @Author: the-ruffian
+ * @Date: 2021-08-30 21:20
+ * @LastEditTime: 2021-8-30 23:04:46
+ * @LastEditors: the-ruffian
+*/
+import axios from "axios";
+
+const service = axios.create({
+    baseURL: 'http://106.13.171.218',
+    timeout: 5000
+})
+service.interceptors.request.use(
+    function (config){
+        const token = localStorage.getItem('token')
+        if (token) {
+            config.headers.authorization = token
+        }
+        if (config.method === 'post' || config.method === 'put') {
+            config.headers.contentType = 'application/json'
+        }
+        return config
+    },
+    error => {
+        return Promise.reject(error)
+    }
+)
+service.interceptors.response.use(function (response: any) {
+    if (response.data.state === false) {
+        localStorage.clear()
+        location.replace('/')
+    } else {
+        return response
+    }
+})
+export const Method:any = {
+    GET: 'get',
+    POST: "post",
+    PUT: 'put',
+    DELETE: 'delete'
+}
+export default service
