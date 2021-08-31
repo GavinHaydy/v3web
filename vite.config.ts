288a1b86e469e2247680260da1d6fd4be322f4b7
bugpz <13558622779@163.com>
2021-08-31 20:46:13 +0800

    fix：添加代理

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://106.13.171.218',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    },
+    open: '/',
+    port: 3000
+  }
 })
