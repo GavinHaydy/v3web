883741267e3604b8d9fe87133f9459df0e2616a0
bugpz <13558622779@163.com>
2021-08-31 18:07:30 +0800

    add：测试页面

---
+++ src/pages/login/login.vue
@@ -0,0 +1,48 @@
+<!--
+ * @Description:login.vue
+ * @CreatedBy:WebStorm
+ * @Author: the-ruffian
+ * @Date: 2021-08-31 11:28
+ * @LastEditTime: 2021-8-31 18:07:24
+ * @LastEditors: the-ruffian
+-->
+<template>
+  <div>
+    <a-button
+        @click="login"
+    >测试</a-button>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, reactive} from "vue";
+import {Md5} from "ts-md5";
+import { userLogin } from "../../api/user";
+
+export default defineComponent({
+  name: 'login',
+  setup() {
+    const form = reactive({
+      phone: '',
+      password: ''
+    })
+    const md5Password = Md5.hashStr(form.password)
+    const login = () =>{
+      userLogin({
+        phone: form.phone,
+        password: md5Password
+      })
+    }
+    return{
+      form,
+      md5Password,
+      login
+    }
+  }
+})
+
+</script>
+
+<style scoped>
+
+</style>
