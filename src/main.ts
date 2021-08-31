14da09b46f05a98da6eb144a9e2517e1dde3698f
bugpz <13558622779@163.com>
2021-08-31 18:05:19 +0800

    add：全局引入antd

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
+import { createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
-createApp(App).use(router).mount('#app')
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+
+createApp(App)
+    .use(router)
+    .use(Antd)
+    .mount('#app')
