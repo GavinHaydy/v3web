9fcc0a0060c93f78b21c8af646e82e487a3abd13
bugpz <13558622779@163.com>
2021-09-01 19:24:42 +0800

    fix：调试登录

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -3,14 +3,34 @@
  * @CreatedBy:WebStorm
  * @Author: the-ruffian
  * @Date: 2021-08-31 11:28
- * @LastEditTime: 2021-8-31 18:07:24
+ * @LastEditTime: 2021-09-01 19:24:01
  * @LastEditors: the-ruffian
 -->
 <template>
   <div>
-    <a-button
-        @click="login"
-    >测试</a-button>
+    <a-card
+      title="欢迎登录"
+    style="width: 40%; margin-left: 30%">
+      <a-form
+          :model="form"
+      >
+        <a-form-item
+            label="账号"
+
+        >
+          <a-input
+              v-model:value="form.phone"
+          />
+        </a-form-item>
+        <a-form-item
+            label="密码">
+          <a-input v-model:value="form.password"/>
+        </a-form-item>
+        <a-button
+            size="small"
+            @click="login">登录</a-button>
+      </a-form>
+    </a-card>
   </div>
 </template>
 
@@ -26,16 +46,14 @@ export default defineComponent({
       phone: '',
       password: ''
     })
-    const md5Password = Md5.hashStr(form.password)
     const login = () =>{
       userLogin({
         phone: form.phone,
-        password: md5Password
+        password: Md5.hashStr(form.password)
       })
     }
     return{
       form,
-      md5Password,
       login
     }
   }
