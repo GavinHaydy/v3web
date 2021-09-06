44e0fca88d726aa18a5393dfda7e852392f1810c
bugpz <13558622779@163.com>
2021-09-06 18:38:30 +0800

    fix：删除无用代码

--- src/global.ts
+++ src/global.ts
@@ -3,16 +3,15 @@
  * @CreatedBy:WebStorm
  * @Author: the-ruffian
  * @Date: 2021-09-05 23:15
- * @LastEditTime: 2021-09-05 23:42:06
+ * @LastEditTime: 2021-9-6 18:37:40
  * @LastEditors: the-ruffian
 */
-import {createApp} from 'vue'
 const checkPhone = /^1[34578][0-9]{9}$/
 const checkPassword = /^[A-Za-z0-9]{8,20}$/
 const checkEmail = /^[a-zA-Z0-9_-]{6,20}@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+$/
 const checkCode = /^[0-9]{4}$/
 const checkNull = /\s/
-const app = createApp({})
+
 export default {
     install (app:any, options:any) {
         app.config.globalProperties.$checkPhone = checkPhone
