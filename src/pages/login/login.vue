5d5bfa208e4559ab3869b0c8d4cf0a58ae15dd72
bugpz <13558622779@163.com>
2021-09-06 19:51:34 +0800

    fix：页面完成

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -3,7 +3,7 @@
  * @CreatedBy:WebStorm
  * @Author: the-ruffian
  * @Date: 2021-08-31 11:28
- * @LastEditTime: 2021-09-01 19:24:01
+ * @LastEditTime: 2021-9-6 19:48:48
  * @LastEditors: the-ruffian
 -->
 <template>
@@ -15,10 +15,12 @@
       >
         <a-form
             :model="form"
+            :rules="rules"
         >
           <a-row>
             <a-form-item
                 label="账号"
+                name="phone"
             >
               <a-input
                   placeholder="请输入手机号"
@@ -28,7 +30,9 @@
           </a-row>
           <a-row>
             <a-form-item
-                label="密码">
+                label="密码"
+                name="password"
+            >
               <a-input
                   placeholder="请输入密码"
                   type="password"
@@ -62,36 +66,59 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, reactive} from "vue";
+import {defineComponent, getCurrentInstance, reactive} from "vue";
 import {Md5} from "ts-md5";
 import { userLogin } from "../../api/user";
 import {message} from "ant-design-vue";
+import { RuleObject } from "ant-design-vue/lib/form/interface";
+
 
 export default defineComponent({
   name: 'login',
   setup() {
+      const internalInstance  = getCurrentInstance()
     const form = reactive({
       phone: '',
       password: ''
     })
-    const login = () =>{
-      userLogin({
-        phone: form.phone,
-        password: Md5.hashStr(form.password)
-      })
-      .then(res =>{
-        if (res.data.code === 200){
-          const token = res.data.result.token
-          localStorage.setItem('token',token)
-          localStorage.setItem('phone', form.phone)
-          localStorage.setItem('username', res.data.result.username)
-          localStorage.setItem('is_login', 'true')
-          location.replace('/default')
-        } else {
-          message.error(res.data.message)
-          setTimeout(function () { location.reload() }, 1500) // 一秒后刷新页面
+    const checkPhone = (rule :RuleObject, value :number) => {
+     if(!internalInstance?.appContext.config.globalProperties.$checkPhone.test(value)){
+       return Promise.reject('请输入正确的手机号')
+     }
+    }
+    const checkPassword = (rule :RuleObject, value :string) =>{
+        if (!internalInstance?.appContext.config.globalProperties.$checkPassword.test(value)){
+          return Promise.reject('密码8-20位，支持英文、数字')
         }
-      })
+    }
+    const login = () =>{
+      if (internalInstance?.appContext.config.globalProperties.$checkPhone.test(form.phone)
+      && internalInstance?.appContext.config.globalProperties.$checkPassword.test(form.password)){
+        userLogin({
+          phone: form.phone,
+          password: Md5.hashStr(form.password)
+        })
+            .then(res =>{
+              if (res.data.code === 200){
+                const token = res.data.result.token
+                localStorage.setItem('token',token)
+                localStorage.setItem('phone', form.phone)
+                localStorage.setItem('username', res.data.result.username)
+                localStorage.setItem('is_login', 'true')
+                location.replace('/default')
+              } else {
+                message.error(res.data.message)
+                setTimeout(function () { location.reload() }, 1500) // 一秒后刷新页面
+              }
+            })
+      }
+      else {
+        message.error('请先填写账号、密码')
+      }
+    }
+    const rules = {
+      phone:[{required: true, validator: checkPhone, trigger: 'blur'}],
+      password: [{required: true, validator: checkPassword, trigger: 'blur'}]
     }
     const Register = () =>{
       location.replace('/register')
@@ -103,7 +130,8 @@ export default defineComponent({
       form,
       login,
       Register,
-      ForgerPassword
+      ForgerPassword,
+      rules
     }
   }
 })
