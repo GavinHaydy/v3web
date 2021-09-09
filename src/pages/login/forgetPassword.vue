c2e9c7448616b231b73e585112bf5edb9c2ad71f
the-ruffian <13558622779@163.com>
2021-09-09 17:31:03 +0800

    fix：逻辑及样式完成

--- src/pages/login/forgetPassword.vue
+++ src/pages/login/forgetPassword.vue
@@ -3,40 +3,38 @@
  * @CreatedBy:WebStorm
  * @Author: the-ruffian
  * @Date: 2021-09-08 17:27
- * @LastEditTime: 2021-9-8 21:21:43
+ * @LastEditTime: 2021-9-9 17:30:24
  * @LastEditors: the-ruffian
 -->
 <template>
-  <div>
+  <div style="padding-top: 35vh">
     <a-card
-        class="card-box">
-      <div
-          slot="header"
-          style="text-align: center"
-      >
-        <h3>忘记密码</h3>
-      </div>
+        style="text-align: center"
+        title="忘记密码"
+        class="card-box"
+    >
       <a-form
-          inline="inline"
           :model="form"
           :rules="rules"
       >
         <a-row>
           <a-form-item
+              class="input"
               name="email">
             <a-input
                 :maxlength="30"
-                v-model="form.email"
+                v-model:value="form.email"
                 placeholder="请输入邮箱地址"></a-input>
           </a-form-item>
         </a-row>
         <a-row>
           <a-form-item
+              class="input"
               name="code"
           >
             <a-input
                 :maxlength="4"
-                v-model="form.code"
+                v-model:value="form.code"
                 placeholder="请输入验证码"></a-input>
           </a-form-item>
           <a-button
@@ -51,22 +49,20 @@
         </a-row>
         <a-row>
           <a-form-item
+              class="input"
               name="password">
-            <a-input
-                v-model="form.password"
-                type="password"
-                show-password
-                placeholder="请输入密码"></a-input>
+            <a-input-password
+                v-model:value="form.password"
+                placeholder="请输入密码"></a-input-password>
           </a-form-item>
         </a-row>
         <a-row>
           <a-form-item
+              class="input"
               name="rePassword">
-            <a-input
-                v-model="form.rePassword"
-                type="password"
-                show-password
-                placeholder="请确认密码"></a-input>
+            <a-input-password
+                v-model:value="form.rePassword"
+                placeholder="请确认密码"></a-input-password>
           </a-form-item>
         </a-row>
         <a-row>
@@ -89,32 +85,41 @@
 </template>
 
 <script setup lang="ts">
-import {getCurrentInstance, reactive, ref} from "vue";
+import {getCurrentInstance, reactive} from "vue";
 import {forgetPassword, getEmailCode} from "../../api/user";
 import {Md5} from "ts-md5";
 import {message} from "ant-design-vue";
+import {RuleObject} from "ant-design-vue/es/form/interface";
 
 const internalInstance = getCurrentInstance()?.appContext.config.globalProperties
-const checkEmail = (value :string) =>{
+const checkEmail = (rule :RuleObject, value :string) =>{
   if (!internalInstance?.$checkEmail.test(value)){
     return Promise.reject('请填写正确的邮箱')
+  } else {
+    return Promise.resolve()
   }
 }
-const checkCode = (value :string) =>{
+const checkCode = (rule :RuleObject, value :any) =>{
   if (!internalInstance?.$checkCode.test(value)){
     return Promise.reject('请输入正确验证码')
+  } else {
+    return Promise.resolve()
   }
 }
-const checkPassword = (value :string) =>{
+const checkPassword = (rule :RuleObject, value :string) =>{
   if (!internalInstance?.$checkPassword.test(value)){
     return Promise.reject('密码8-20位，支持英文、数字')
+  } else {
+    return Promise.resolve()
   }
 }
-const checkRepass = (value :string) => {
+const checkRepass = (rule :RuleObject, value :string) => {
   if (value !== form.password) {
     return Promise.reject('两次密码不一致，请重新输入')
   } else if (!internalInstance?.$checkPassword.test(value)) {
     return Promise.reject('密码8-20位，支持英文、数字')
+  } else {
+    return Promise.resolve()
   }
 }
 const form = reactive({
@@ -134,7 +139,10 @@ let getCodeTime = reactive({
   show: true
 })
 const handleSubmit = () =>{
-  if (form.password === form.rePassword){
+  if (form.password === form.rePassword &&
+      internalInstance?.$checkEmail.test(form.email) &&
+      internalInstance?.$checkCode.test(form.code) &&
+      internalInstance?.$checkPassword.test(form.password)){
     forgetPassword({
       'password': Md5.hashStr(form.password),
       'rePassword': Md5.hashStr(form.password),
@@ -149,6 +157,8 @@ const handleSubmit = () =>{
         message.error(res.data.message)
       }
     })
+  } else {
+    message.warning('请按要求填写表单')
   }
 }
 const handleBack = () => {
@@ -171,6 +181,8 @@ const getCode = () => {
         },1000)
       }
     })
+  } else {
+    message.error('cs')
   }
 }
 </script>
@@ -180,7 +192,6 @@ const getCode = () => {
   background-color: #f9a7a7;
   width: 36%;
   height: 45vh;
-  margin-top: 30vh;
   margin-left: 32%;
 }
 </style>
