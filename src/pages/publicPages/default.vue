d08021fac449528c1f97b952427e091780b460f2
bugpz <13558622779@163.com>
2021-09-13 20:55:51 +0800

    add：首页

---
+++ src/pages/publicPages/default.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Description:default.vue
+ * @CreatedBy:WebStorm
+ * @Author: the-ruffian
+ * @Date: 2021-09-13 20:45
+ * @LastEditTime: 2021-09-13 20:45
+ * @LastEditors: the-ruffian
+-->
+<template>
+  <div>
+    welcome
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
