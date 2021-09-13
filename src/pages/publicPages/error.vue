dd2ad0e8389b6cd0dfe6c845029f789c602f3bef
bugpz <13558622779@163.com>
2021-09-13 21:22:26 +0800

    add：错误页

---
+++ src/pages/publicPages/error.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Description:error.vue
+ * @CreatedBy:WebStorm
+ * @Author: the-ruffian
+ * @Date: 2021-09-13 21:05
+ * @LastEditTime: 2021-09-13 21:05
+ * @LastEditors: the-ruffian
+-->
+<template>
+  <div id="ErrorPage">
+    <iframe src="../../static/404.html" style="width: 100%">测试</iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
