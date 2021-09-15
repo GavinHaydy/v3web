c479cdead22cf359512622a4555c238b9d8d783c
bugpz <13558622779@163.com>
2021-09-15 22:52:47 +0800

    fix：修改文件路径，error页样式增加高度，解决404页面找不到的问题

--- src/pages/publicPages/error.vue
+++ src/pages/publicPages/error.vue
@@ -3,12 +3,12 @@
  * @CreatedBy:WebStorm
  * @Author: the-ruffian
  * @Date: 2021-09-13 21:05
- * @LastEditTime: 2021-09-13 21:05
+ * @LastEditTime: 2021-09-15 22:52:42
  * @LastEditors: the-ruffian
 -->
 <template>
   <div id="ErrorPage">
-    <iframe src="../../static/404.html" style="width: 100%">测试</iframe>
+    <iframe src="../../../public/404.html" style="width: 100%;height: 100vh">测试</iframe>
   </div>
 </template>
 
