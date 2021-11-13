53683eab6d06480e8df7be5aae6d5c4990b3ff77
bugpz <13558622779@163.com>
2021-11-13 18:45:10 +0800

    fix：设置高亮语言

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import prismjsPlugin from "vite-plugin-prismjs";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+  prismjsPlugin({
+    languages: 'all'
+  })],
   server: {
     proxy: {
       '/api': {
