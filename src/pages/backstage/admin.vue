0e312f73568c23c8ec4131087942d71947f56071
bugpz <13558622779@163.com>
2021-11-15 18:24:25 +0800

    fix：后台管理页面

---
+++ src/pages/backstage/admin.vue
@@ -0,0 +1,56 @@
+<template>
+  <a-layout style="height: 100vh">
+    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+        <a-menu-item key="1">
+          <user-outlined />
+          <span>nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <video-camera-outlined />
+          <span>nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <menu-unfold-outlined
+            v-if="collapsed"
+            class="trigger"
+            @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+        <span class="right">退出</span>
+      </a-layout-header>
+      <a-layout-content
+          :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+      >
+        Content
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script setup lang="ts">
+import {UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,MenuFoldOutlined,MenuUnfoldOutlined} from "@ant-design/icons-vue";
+import {reactive, ref} from "vue";
+
+components:{
+  MenuFoldOutlined
+  MenuUnfoldOutlined
+  UserOutlined
+  VideoCameraOutlined
+  UploadOutlined}
+const collapsed = ref<boolean>(false)
+</script>
+
+<style scoped>
+
+</style>
