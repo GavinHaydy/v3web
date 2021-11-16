2f1ad144900e0809d7293d61432e40e54c87911e
bugpz <13558622779@163.com>
2021-11-16 17:40:11 +0800

    fix：文章编辑页

---
+++ src/pages/publicPages/editArticle.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Description:editArticle.vue
+ * @CreatedBy:WebStorm
+ * @Author: the-ruffian
+ * @Date: 2021-09-13 20:45
+ * @LastEditTime: 2021-09-13 20:45
+ * @LastEditors: the-ruffian
+-->
+<template>
+  <a-row>
+    <a-col :span="2">
+      <h3 style="text-align: center">标题</h3>
+    </a-col>
+    <a-col :span="12">
+      <a-input class="a-input"></a-input>
+    </a-col>
+    <a-button type="primary" @click="save">发布</a-button>
+  </a-row>
+  <v-md-editor v-model="data.text" @save="save"></v-md-editor>
+</template>
+
+<script setup lang="ts">
+import {reactive} from "vue";
+
+const data = reactive({
+  text: ''
+})
+const save = () =>{
+  console.log(typeof(data.text), data.text)
+}
+</script>
+
+<style scoped>
+.a-input {
+  border-width: 0px;
+  border-bottom-width: 1px;
+  border-bottom-color: coral;
+}
+</style>
