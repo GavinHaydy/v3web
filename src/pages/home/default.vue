c1b8b327935afe277910cc3dc9fce5fc47a3c972
bugpz <13558622779@163.com>
2021-12-15 15:52:24 +0800

    fix：默认页修改

---
+++ src/pages/home/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="height: 4vh;background-color: #f9a7a7"></div>
+  <div style="height: 96vh;background-color: antiquewhite">
+    <div v-for="i in data.i">{{ i ** 5 }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {reactive} from "vue";
+
+const data = reactive({
+  i: 20
+})
+</script>
+
+<style >
+
+</style>
