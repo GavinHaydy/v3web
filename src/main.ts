cd1a50d515c9f192bad78c75053eb140b72b9853
bugpz <13558622779@163.com>
2021-12-31 14:53:43 +0800

    fix：增加md预览组件

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,24 @@ import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import './assets/css/base.css'
 import G from "./global";
+
+//md编辑组件
 import VueMarkdownEditor from '@kangc/v-md-editor';
 import '@kangc/v-md-editor/lib/style/base-editor.css';
 import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
 import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
 import Prism from 'prismjs'
 
+//md预览组件
+import VMdPreview from '@kangc/v-md-editor/lib/preview'
+import '@kangc/v-md-editor/lib/style/preview.css';
+
+
 //引入所有语言包
 import '@kangc/v-md-editor/lib/theme/style/github.css'
+import '@kangc/v-md-editor/lib/style/preview.css';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+
 
 VueMarkdownEditor.use(vuepressTheme,{
     Prism,
@@ -20,9 +30,13 @@ VueMarkdownEditor.use(vuepressTheme,{
         // md为 markdown-it 实例，可以在此处进行修改配置,并使用 plugin 进行语法扩展
     },
 });
+VMdPreview.use(vuepressTheme,{
+    Prism
+})
 createApp(App)
     .use(router)
     .use(Antd)
     .use(VueMarkdownEditor)
+    .use(VMdPreview)
     .use(G)
     .mount('#app')
