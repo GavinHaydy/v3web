af5cca86b95e9eee3805287d772095004635c6ba
bugpz <13558622779@163.com>
2021-12-31 14:50:06 +0800

    fix：增加三方库文件声明

--- src/env.d.ts
+++ src/env.d.ts
@@ -10,3 +10,5 @@ declare module '@kangc/v-md-editor';
 declare module '@kangc/v-md-editor/lib/theme/vuepress.js'
 declare module 'prismjs'
 declare module '@kangc/v-md-editor/lib/theme/github.js'
+
+declare module '@kangc/v-md-editor/lib/preview'
