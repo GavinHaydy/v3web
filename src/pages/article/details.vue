8cca6c4c30180637fa3f504fdd3d961acedafbe7
bugpz <13558622779@163.com>
2021-12-31 14:55:22 +0800

    feat：文章详情页

---
+++ src/pages/article/details.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-md-preview
+      :text=listTable.list.article
+  ></v-md-preview>
+</template>
+
+<script setup lang="ts">
+import {reactive} from "vue";
+import {useRoute} from "vue-router";
+import {articleBody} from "../../api/article";
+import {message} from "ant-design-vue";
+
+
+const listTable = reactive({
+  list:''
+})
+const getBody = () => {
+  articleBody({
+    'id': useRoute().query.id
+  }).then(res =>{
+    if (res.data.code ===200){
+      listTable.list = res.data.result
+    }else {
+      message.error(res.data.message)
+    }
+  })
+}
+methods: {
+  getBody()
+}
+</script>
+
+<style scoped>
+
+</style>
